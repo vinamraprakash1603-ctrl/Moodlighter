b5018c445a9fb9ac9830315b37194691c53ba841
vinamraprakash1603-ctrl <vinamraprakash1603@gmail.com>
2026-05-03 00:22:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ const CONFIG = {
     },
     {
       day:        2,
-      unlocksAt:  "2025-05-05T00:00:00",
+      unlocksAt:  "2026-05-05T00:00:00",
       teaserMsg:  "Hii Babu, Abhi time nahi hua hai na open krne ka, Have Patience my baby. 🌸",
       images:     ["0201.jpg", "0202.jpg"],
       voices:     [],
